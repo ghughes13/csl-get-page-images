d4b30588cf06e333ea007220b7eea62442f9d1eb
ghughes13 <battletank130@gmail.com>
2023-06-13 11:19:28 -0500

    refactored code so that content_scripts are no longer needed

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,26 @@
+function getImages() {
+  let imgUrls = [];
+
+  document.querySelectorAll("img").forEach((img) => {
+    imgUrls.push(img.src);
+  });
+
+  chrome.storage.local.clear();
+
+  chrome.storage.local.set({ key: imgUrls }).then(() => {
+    console.log("Value is set");
+  });
+}
+
 chrome.action.onClicked.addListener((tab) => {
-  console.log("sending message");
-  chrome.tabs.sendMessage(tab.id, { message: "browser action" });
-
-  function ShowNewPage() {
-    chrome.tabs.create({ url: `index.html` }, function (tab) {});
+  if (!tab.url.includes("chrome://")) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tab.id },
+        function: getImages,
+      })
+      .then(() => {
+        chrome.tabs.create({ url: `index.html` });
+      });
   }
-
-  ShowNewPage();
 });
